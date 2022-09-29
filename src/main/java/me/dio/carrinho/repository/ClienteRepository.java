1b5ce3c588c4faa6a7c2d894564dc215764234be
AnaSpinetti <ana-spinetti@hotmail.com>
2022-09-29 13:58:16 -0300

    Corrigido arquivo .yml (identação) e criados repositories extendendo o JPARepository

---
+++ src/main/java/me/dio/carrinho/repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package me.dio.carrinho.repository;
+
+import me.dio.carrinho.model.Carrinho;
+import me.dio.carrinho.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
